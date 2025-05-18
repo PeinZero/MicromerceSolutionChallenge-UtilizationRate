44866ae7f609b8007df340eccd557c0defa66cea
Mahad Khalid <mahadzx@gmail.com>
2025-05-18 17:33:43 +0200

    completed the task

--- src/table-script.tsx
+++ src/table-script.tsx
@@ -7,6 +7,26 @@ import { useMemo } from "react";
 import sourceData from "./source-data.json";
 import type { SourceDataType, TableDataType } from "./types";
 
+
+const data = sourceData as unknown as SourceDataType[];
+
+const activeEmployeesData  = data.filter(
+  (dataRow) => dataRow?.employees && dataRow?.employees?.statusAggregation?.status === "Aktiv"
+);
+
+// employmentStatus type should be a string literal "active" or "inactive" or something.
+const activeExternalsData  = data.filter(
+  (dataRow) => dataRow?.externals && dataRow?.externals?.employmentStatus?.employmentStatus === "Aktiv"
+);
+
+let filteredData: SourceDataType[] = [];
+if(activeExternalsData.length > 0){
+  filteredData = [...activeEmployeesData, ...activeExternalsData];
+}
+else{
+  filteredData = activeEmployeesData;
+}
+
 /**
  * Example of how a tableData object should be structured.
  *
@@ -19,20 +39,28 @@ import type { SourceDataType, TableDataType } from "./types";
  * @prop {number} july - The value for July.
  * @prop {number} netEarningsPrevMonth - The net earnings for the previous month.
  */
+const tableData: TableDataType[] =  filteredData.map((dataRow, index) => {
+  
+  const data = dataRow.employees || dataRow.externals;
 
-const tableData: TableDataType[] = (
-  sourceData as unknown as SourceDataType[]
-).map((dataRow, index) => {
-  const person = `${dataRow?.employees?.firstname} - ...`;
+  const person = data?.name;
+  const {utilisationRateLastTwelveMonths, utilisationRateYearToDate, lastThreeMonthsIndividually} = data!.workforceUtilisation || {};
+
+  // May Utilisation is not available in the data !!
+  const mayUtilisationRate = (lastThreeMonthsIndividually?.find(data => data.month === "May"))?.utilisationRate ?? "0.00";
+  const juneUtilisationRate = (lastThreeMonthsIndividually?.find(data => data.month === "June"))?.utilisationRate ?? "0.00";
+ const julyUtilisationRate = (lastThreeMonthsIndividually?.find(data => data.month === "July"))?.utilisationRate ?? "0.00";
+
+  const monthlySalary = data?.statusAggregation?.monthlySalary || "N/A";
 
   const row: TableDataType = {
     person: `${person}`,
-    past12Months: `past12Months ${index} placeholder`,
-    y2d: `y2d ${index} placeholder`,
-    may: `may ${index} placeholder`,
-    june: `june ${index} placeholder`,
-    july: `july ${index} placeholder`,
-    netEarningsPrevMonth: `netEarningsPrevMonth ${index} placeholder`,
+    past12Months: `${Number(utilisationRateLastTwelveMonths)*100} %`,
+    y2d: `${Number(utilisationRateYearToDate)*100} %`,
+    may: `${Number(mayUtilisationRate)*100} %`,
+    june: `${Number(juneUtilisationRate)*100} %`,
+    july: `${Number(julyUtilisationRate)*100} %`,
+    netEarningsPrevMonth: `${monthlySalary} EUR`,
   };
 
   return row;
